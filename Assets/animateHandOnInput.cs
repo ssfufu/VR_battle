970323766910078fba75ad55f0715df78e2ae74b
Mathieu <48691168+ssfufu@users.noreply.github.com>
2023-01-30 21:09:13 +0100

    first commit

---
+++ Assets/animateHandOnInput.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class animateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty pichAnimationAction;
+    public Animator handAnimator;
+    public InputActionProperty gripAnimationAction;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = pichAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+    }
+}
