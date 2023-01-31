2cd81954cec96f06bbf1988e6f52d523761aa187
Mathieu <48691168+ssfufu@users.noreply.github.com>
2023-01-31 21:52:35 +0100

    ennemy models & anims

--- Assets/_Scripts/ennemyAI.cs
+++ Assets/_Scripts/ennemyAI.cs
@@ -6,20 +6,50 @@ using UnityEngine.AI;
 public class ennemyAI : MonoBehaviour
 {
     [SerializeField] private GameObject player;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float attackDelay = 3f;
     private NavMeshAgent nma;
-
-
+    private float t = 0;
     
     void Start()
     {
         nma = GetComponent<NavMeshAgent>();
+        nma.isStopped = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        nma.SetDestination(player.transform.position);
+        Movement(); // Move to player
+        Animation(); // Set animation
+        Attack(); // Attack player
+    }
 
-        
+    private bool isInRange() // Check if player is in range
+    {
+        return nma.hasPath && nma.remainingDistance < nma.stoppingDistance;
+    }
+
+    private void Movement ()
+    {
+        nma.SetDestination(player.transform.position);
+    }
+
+    private void Animation ()
+    {
+        animator.SetBool("inRange", isInRange());
+    }
+
+    public void Attack()
+    {
+        if (isInRange())
+        {
+            t += Time.deltaTime;
+            if (t >= attackDelay)
+            {
+                t = 0;
+                animator.SetTrigger("Attack");
+            }
+        }
     }
 }
